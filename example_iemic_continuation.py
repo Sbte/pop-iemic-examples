3ef88cf61f8caadd58b1f3237073f9edcc958de9
Sven Baars <s.baars@uu.nl>
2023-02-06 12:03:35 +0100

    example_iemic_continuation: Make the iemic continuation compatible with new interfaces.

--- example_iemic_continuation.py
+++ example_iemic_continuation.py
@@ -1,46 +1,49 @@
-from iemic import initialize_global_iemic
+import iemic
 
 from fvm import Continuation
 
-from omuse.io import write_set_to_file
 
-if __name__=="__main__":
-    instance=initialize_global_iemic()
+def run_continuation(target=1.0):
+    instance = iemic.initialize_global_iemic()
 
     print("starting")
 
     # the following line optionally redirects iemic output to file
-    #~ instance.set_output_file("output.%p")
+    # ~ instance.set_output_file("output.%p")
 
-    #print out all initial parameters
+    # print out all initial parameters
     print(instance.parameters)
-    
+
     x = instance.get_state()
 
     # print out actually used THCM parameters
     print(instance.Ocean__THCM__Starting_Parameters)
-    
 
     # numerical parameters for the continuation
-    parameters={"Newton Tolerance" : 1.e-2, "Verbose" : True,
-                "Minimum Step Size" : 0.001,
-                "Maximum Step Size" : 0.2,
-                "Delta" : 1.e-6 }
+    parameters = {
+        "Newton Tolerance": 1.0e-2,
+        "Verbose": True,
+        "Minimum Step Size": 0.001,
+        "Maximum Step Size": 0.2,
+        "Delta": 1.0e-6,
+    }
 
     # setup continuation object
-    continuation=Continuation(instance, parameters)
-    
-    # Converge to an initial steady state
-    x = continuation.newton(x, 1e-10)
-      
+    continuation = Continuation(instance, parameters)
+
     print("start continuation, this may take a while")
 
-    x, mu, data = continuation.continuation(x, 'Ocean->THCM->Starting Parameters->Combined Forcing', 0., 1., 0.005)
+    ds = 0.005
+    x, mu = continuation.continuation(x, "Ocean->THCM->Starting Parameters->Combined Forcing", 0.0, target, ds)
 
-    write_set_to_file(x.grid, "global_96x38x12.amuse","amuse", overwrite_file=True)
+    iemic.save_iemic_state(instance, 'global_state')
 
     print("continuation done")
 
     instance.stop()
-        
+
     print("done")
+
+
+if __name__ == "__main__":
+    run_continuation()
